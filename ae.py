aa8338f7014b45437062ede653b86da003170ad1
Jimmy Chung <jimmy46@hotmail.ca>
2020-01-25 00:53:35 -0500

    1.0

--- ae.py
+++ ae.py
@@ -86,13 +86,13 @@ test_loss = 0
 s = 0.
 for id_user in range(nb_users):
     input = Variable(training_set[id_user]).unsqueeze(0)
-    target = Variable(test_set[id_user])
+    target = Variable(test_set[id_user]).unsqueeze(0)
     if torch.sum(target.data > 0) > 0:
         output = sae(input)
         target.require_grad = False
         output[target == 0] = 0
         loss = criterion(output, target)
         mean_corrector = nb_movies/float(torch.sum(target.data > 0) + 1e-10)
-        test_loss += np.sqrt(loss.data[0]*mean_corrector)
+        test_loss += np.sqrt(loss.data*mean_corrector)
         s += 1.
 print('test loss: '+str(test_loss/s))
